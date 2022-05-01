6163f60cd09964a9638b0dee32e0835275d89a66
AlexanderRubin <alexander_rubin_29@hotmail.com>
2022-05-01 00:24:26 -0300

    agregar lista de instrucciones con lista simplemente enlazada

--- Consola/main.c
+++ Consola/main.c
@@ -1,17 +1,53 @@
 
 #include "include/main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Nodo{
+	char *Codigo;
+	int  Parametro1;
+	int  Parametro2;
+	struct Nodo *sig;
+} Node;
+
+typedef struct ListaIdentificar {
+	Node *inicio;
+	Node *fin;
+	int tamanio;
+}Lista;
+
+int Inicializar(Lista*);
+int Agregar(Lista*, Node*, struct Instruccion);
+void Imprimir(Lista*);
 
 t_log* consola_logger;
-
 
 int main(){
 	//log_create(char* file, char *process_name, bool is_active_console, t_log_level level)
 	consola_logger = log_create("consola.log","CONSOLA",1,LOG_LEVEL_DEBUG);
 
 	struct Instruccion lista_inst;
-	FILE* archivo=fopen("proceso1.txt","r");
-	struct Instruccion hola = parser(archivo);
-	printf(hola.codigo_instruccion); //esto es a modo de ejemplo
+	Lista* lista;
+	Node* actual;
+	actual = NULL;
+	int resultAgregar = 0;
+	int resultIniciar = 0;
+
+ 	resultIniciar = Inicializar(lista);
+
+	if (resultIniciar == -1)
+		 exit(EXIT_FAILURE);
+
+	FILE* archivo = fopen("proceso1.txt","r");
+	while(!feof(archivo) ){
+		struct Instruccion InstruccionLeida = parser(archivo);
+		resultAgregar = Agregar(lista, lista->fin, InstruccionLeida);	
+	}
+
+	Imprimir(lista);
+	fclose(archivo);
+	
 
 	//Luego, leerá su archivo de configuración, se conectará al Kernel y le enviará toda la información del proceso (lista de instrucciones, tamaño).
 
@@ -27,53 +63,84 @@ int main(){
 	send(consola_fd, &handshake, sizeof(uint32_t), 0);
 	recv(consola_fd, &result, sizeof(uint32_t), MSG_WAITALL);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 	return 0;
 
 }
 
-int crear_socket(char* ip_kernel,char* puerto_kernel){
-	struct addrinfo hints;
-	struct addrinfo *server_info;
+int Inicializar (Lista *lista){
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	//hints.ai_flags = AI_PASSIVE;
+if ((lista = (Lista *) malloc (sizeof (Lista))) == NULL)
+		return -1;
+  lista->inicio = NULL;
+  lista->fin = NULL;
+  lista->tamanio = 0;
+  return 0;
+}
 
-	getaddrinfo(ip_kernel, puerto_kernel, &hints, &server_info);
-
-
-	int consola_fd = socket(server_info->ai_family, server_info->ai_socktype,server_info->ai_protocol);
-	if(connect(consola_fd, server_info->ai_addr, server_info->ai_addrlen)==-1){
-		log_info(consola_logger,"Error al conectar el socket");
+int Agregar(Lista* lista, Node* actual, struct Instruccion dato)
+{
+	Node *nuevo_nodo;
+	nuevo_nodo = (Node *) malloc(sizeof(Node));
+	
+	if(nuevo_nodo == NULL)
+	{
 		return -1;
 	}
-	freeaddrinfo(server_info);
-	return consola_fd;
+	if ((nuevo_nodo->Codigo = (char *) malloc (50 * sizeof (char))) == NULL) 
+	{
+		return -1;
+	}
+		//Copio los datos al nuevo nodo
+		strcpy (nuevo_nodo->Codigo, dato.codigo_instruccion);
+		nuevo_nodo->Parametro1 = dato.parametros[0];
+		nuevo_nodo->Parametro2 = dato.parametros[1];
+		nuevo_nodo->sig = NULL;	
+		
+		//Insertar al final
+		actual->sig = nuevo_nodo; 
+		nuevo_nodo->sig = NULL; 
+		lista->fin = nuevo_nodo; 
+		lista->tamanio++; 
+	
+	return 0;
 }
+
+
+void Imprimir (Lista * lista){
+  Node *actual;
+  actual = lista->inicio;
+  printf("\nLista:\n\n");
+  
+  while (actual != NULL){
+      printf ("%s\n", actual->Codigo);
+      //printf ("%d\n", actual.Parametro1);
+      //printf ("%d\n", actual.Parametro2);
+      actual = actual->sig;
+  }
+}
+
+
+ int crear_socket(char* ip_kernel,char* puerto_kernel){
+ 	struct addrinfo hints;
+ 	struct addrinfo *server_info;
+
+ 	memset(&hints, 0, sizeof(hints));
+ 	hints.ai_family = AF_UNSPEC;
+ 	hints.ai_socktype = SOCK_STREAM;
+ 	//hints.ai_flags = AI_PASSIVE;
+
+ 	getaddrinfo(ip_kernel, puerto_kernel, &hints, &server_info);
+
+
+ 	int consola_fd = socket(server_info->ai_family, server_info->ai_socktype,server_info->ai_protocol);
+ 	if(connect(consola_fd, server_info->ai_addr, server_info->ai_addrlen)==-1){
+ 		log_info(consola_logger,"Error al conectar el socket");
+ 		return -1;
+ 	}
+ 	freeaddrinfo(server_info);
+ 	return consola_fd;
+ }
+
 
 
 
