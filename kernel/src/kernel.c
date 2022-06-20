a619ee1118d5dc5e4e24a4c5e3818b6167e8b15b
JuaaannCruuuzz <juancruzcaggiano@hotmail.com>
2022-06-20 18:00:41 -0300

    vuelvo a poner kernel.c

---
+++ kernel/src/kernel.c
@@ -0,0 +1,41 @@
+#include "kernel.h"
+
+int main(int argc, char* argv[]) {
+    kernelCfg = kernel_cfg_create();
+    kernelLogger = log_create(KERNEL_LOG_DEST, KERNEL_MODULE_NAME, true, LOG_LEVEL_INFO);
+    cargar_configuracion(KERNEL_MODULE_NAME, kernelCfg, KERNEL_CFG_PATH, kernelLogger, kernel_config_initialize);
+
+    int socketEscucha = iniciar_servidor(kernelCfg->IP_KERNEL, kernelCfg->PUERTO_ESCUCHA);
+    
+    struct sockaddr cliente;
+    socklen_t len = sizeof(cliente);
+
+    iniciar_planificacion(); //esta se conecta a cpu
+
+    aceptar_conexiones_kernel(socketEscucha, cliente, len);
+
+    liberar_modulo_kernel(kernelLogger, kernelCfg);
+
+    return EXIT_SUCCESS;
+}
+
+void aceptar_conexiones_kernel(int socketEscucha, struct sockaddr cliente, socklen_t len) {
+    log_info(kernelLogger, "Kernel: A la escucha de nuevas conexiones en puerto %d", socketEscucha);
+    for(;;) {
+        kernelCfg->CONSOLA_SOCKET = accept(socketEscucha, &cliente, &len); //esta mal
+        if(kernelCfg->CONSOLA_SOCKET > 0) {
+            log_info(kernelLogger, "Kernel: Acepto la conexión del socket: %d", kernelCfg->CONSOLA_SOCKET);
+            crear_hilo_handler_conexion_entrante(kernelCfg->CONSOLA_SOCKET);
+        } else {
+            log_error(kernelLogger, "Kernel: Error al aceptar conexión: %s", strerror(errno));
+        }
+    }
+}
+
+void crear_hilo_handler_conexion_entrante(int* socket) 
+{
+    pthread_t threadSuscripcion;
+    pthread_create(&threadSuscripcion, NULL, agregar_pcb_en_cola_new, (void *) socket);
+    pthread_detach(threadSuscripcion);
+    return;
+}
