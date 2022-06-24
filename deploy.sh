6257fa4f1e212de7e4ac177583a163312aac23f9
JuaaannCruuuzz <juancruzcaggiano@hotmail.com>
2022-06-24 18:20:12 -0300

    Se agrega creacion de carpeta SWAP, para archivos

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir utnso
+cd utnso
 CWD=$PWD
 echo -e "\n\nInstalando las commons...\n\n"
 COMMONS="so-commons-library"
@@ -8,6 +10,7 @@ sudo make uninstall
 make all
 sudo make install
 cd $CWD
+mkdir swap
 echo -e "\n\nBuildeando el proyecto\n\n"
 make all
 echo -e "\n\nDeploy done!\n\n"
