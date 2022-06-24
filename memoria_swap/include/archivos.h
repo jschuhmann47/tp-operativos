29693450e0ff629b7c0c6f158936c76226113f06
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2022-06-23 21:24:37 -0300

    agrego funciones de archivos, sin probar

--- memoria_swap/include/archivos.h
+++ memoria_swap/include/archivos.h
@@ -9,9 +9,11 @@
 #include <commons/string.h>
 #include "memoria_swap_config.h"
 
-void generar_archivo(uint32_t pid, char *contenido);
-void eliminar_archivo(uint32_t pid);
-void escribir_en_archivo(uint32_t pid, char *contenido);
 
+void generar_archivo(uint32_t pid);
+void eliminar_archivo(uint32_t pid);
+void escribir_en_archivo(uint32_t pid, int nroMarco);
+void* leer_de_archivo(uint32_t pid,int nroMarco);
+char* obtener_path_archivo(uint32_t pid);
 
 #endif
--- memoria_swap/src/archivos.c
+++ memoria_swap/src/archivos.c
@@ -5,19 +5,11 @@ pthread_mutex_t accesoAArchivo;
 
 
 
-void generar_archivo(uint32_t pid, char *contenido)
+void generar_archivo(uint32_t pid)
 {
     pthread_mutex_lock(&accesoAArchivo);
-    char *path = string_new();
-    string_append(&path, memoria_swapCfg->PATH_SWAP);
-    string_append(&path, string_itoa(pid));
-    string_append(&path, ".swap");
-    FILE *archivo = fopen(path, "w"); //a+: append y read
-    
-    //escribir lo que haya que escribir
-    
-    //fseek(archivo, (pagina * tamanio), SEEK_SET);
-    //fwrite(contenido, sizeof(contenido)+1, tamanio, archivo);
+    char* path = obtener_path_archivo(pid);
+    FILE *archivo = fopen(path, "w");
     fclose(archivo);
     pthread_mutex_unlock(&accesoAArchivo);
 }
@@ -25,17 +17,42 @@ void generar_archivo(uint32_t pid, char *contenido)
 void eliminar_archivo(uint32_t pid)
 {
     pthread_mutex_lock(&accesoAArchivo);
-    char *path = string_new();
-    string_append(&path, memoria_swapCfg->PATH_SWAP);
-    string_append(&path, string_itoa(pid));
-    string_append(&path, ".swap");
+    char* path = obtener_path_archivo(pid);
     remove(path);
     pthread_mutex_unlock(&accesoAArchivo);
 }
 
-void escribir_en_archivo(uint32_t pid, char *contenido){
+void escribir_en_archivo(uint32_t pid, int nroMarco){
+    char* path = obtener_path_archivo(pid);
     pthread_mutex_lock(&accesoAArchivo);
-    //TODO
+    FILE *archivo = fopen(path, "w");
+    void* paqueteAEscribir = leer_de_memoria(MEMORIA_PRINCIPAL,nroMarco,0,memoria_swapCfg->TAM_PAGINA);
+    fseek(archivo,nroMarco*memoria_swapCfg->TAM_PAGINA,SEEK_SET);
+    fwrite(paqueteAEscribir,memoria_swapCfg->TAM_PAGINA,1,archivo);
+    //ver si esto anda, tmb existe mmap() pero hay que ver como se usa
+    fseek(archivo,0,SEEK_SET);
+    fclose(archivo);
+    pthread_mutex_unlock(&accesoAArchivo);
+}
+
+void* leer_de_archivo(uint32_t pid,int nroMarco){
+    pthread_mutex_lock(&accesoAArchivo);
+    char* path = obtener_path_archivo(pid);
+    FILE *archivo = fopen(path, "r");
+    void* lectura;
+    fseek(archivo,nroMarco*memoria_swapCfg->TAM_PAGINA,SEEK_SET);
+    fread(lectura,memoria_swapCfg->TAM_PAGINA,1,archivo);
+    fseek(archivo,0,SEEK_SET);
+    fclose(archivo);
     pthread_mutex_unlock(&accesoAArchivo);
 
+    return lectura; //ver como formatear esto, es el contenido del frame tal cual
+}
+
+char* obtener_path_archivo(uint32_t pid){
+    char *path = string_new();
+    string_append(&path, memoria_swapCfg->PATH_SWAP);
+    string_append(&path, string_itoa(pid));
+    string_append(&path, ".swap");
+    return path;
 }
