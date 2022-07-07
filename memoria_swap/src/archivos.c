e838092a6fa51dea9b4a71346170a594c793923b
JuaaannCruuuzz <juancruzcaggiano@hotmail.com>
2022-07-07 20:01:53 -0300

    Cambiada la funcion de generar_archivo

--- memoria_swap/src/archivos.c
+++ memoria_swap/src/archivos.c
@@ -1,12 +1,29 @@
 #include "archivos.h"
+#include <unistd.h>
 
 pthread_mutex_t accesoAArchivo;
+
+// void generar_archivo(uint32_t pid)
+// {
+//     pthread_mutex_lock(&accesoAArchivo);
+//     char* path = obtener_path_archivo(pid);
+//     FILE *archivo = fopen(path, "w");
+//     fclose(archivo);
+//     pthread_mutex_unlock(&accesoAArchivo);
+// }
 
 void generar_archivo(uint32_t pid)
 {
     pthread_mutex_lock(&accesoAArchivo);
     char* path = obtener_path_archivo(pid);
-    FILE *archivo = fopen(path, "w");
+    FILE* archivo = fopen(path, "w");
+    if (ftruncate(fileno(archivo), memoria_swapCfg->TAM_PAGINA*memoria_swapCfg->PAGINAS_POR_TABLA*memoria_swapCfg->PAGINAS_POR_TABLA+1) != 0){
+        log_error(memoria_swapLogger,"Error al truncate");
+        exit(-1);
+    }
+    fseek(archivo, 1, SEEK_END);
+    fwrite("\0", 1, 1, archivo);
+    fseek(archivo, 0, SEEK_SET);
     fclose(archivo);
     pthread_mutex_unlock(&accesoAArchivo);
 }
@@ -19,14 +36,13 @@ void eliminar_archivo(uint32_t pid)
     pthread_mutex_unlock(&accesoAArchivo);
 }
 
-void escribir_en_archivo(uint32_t pid, int nroMarco)
+void escribir_en_archivo(uint32_t pid, int nroMarco, int nroPagina)
 {
     char* path = obtener_path_archivo(pid);
     pthread_mutex_lock(&accesoAArchivo);
-    FILE *archivo = fopen(path, "w");
+    FILE *archivo = fopen(path, "wb");
     void* paqueteAEscribir = leer_de_memoria(MEMORIA_PRINCIPAL,nroMarco,0,memoria_swapCfg->TAM_PAGINA);
-    log_info(memoria_swapLogger,"MARCO RECIBIDO %i",nroMarco);
-    fseek(archivo,nroMarco*memoria_swapCfg->TAM_PAGINA,SEEK_SET);
+    fseek(archivo,nroPagina*memoria_swapCfg->TAM_PAGINA,SEEK_SET);
     fwrite(paqueteAEscribir,memoria_swapCfg->TAM_PAGINA,1,archivo);
     //ver si esto anda, tmb existe mmap() pero hay que ver como se usa
     fseek(archivo,0,SEEK_SET);
@@ -34,12 +50,12 @@ void escribir_en_archivo(uint32_t pid, int nroMarco)
     pthread_mutex_unlock(&accesoAArchivo);
 }
 
-void* leer_de_archivo(uint32_t pid,int nroMarco){
+void* leer_de_archivo(uint32_t pid,int nroPagina){
     pthread_mutex_lock(&accesoAArchivo);
     char* path = obtener_path_archivo(pid);
     FILE *archivo = fopen(path, "r");
     void* lectura = malloc(memoria_swapCfg->TAM_PAGINA);
-    fseek(archivo,nroMarco*memoria_swapCfg->TAM_PAGINA,SEEK_SET);
+    fseek(archivo,nroPagina*memoria_swapCfg->TAM_PAGINA,SEEK_SET);
     fread(lectura,memoria_swapCfg->TAM_PAGINA,1,archivo);
     fseek(archivo,0,SEEK_SET);
     fclose(archivo);
