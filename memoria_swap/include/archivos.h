97654172fcbcabeb82446e07773f2bd6c603647d
JuaaannCruuuzz <juancruzcaggiano@hotmail.com>
2022-07-07 20:00:16 -0300

    Definicion de funciones

--- memoria_swap/include/archivos.h
+++ memoria_swap/include/archivos.h
@@ -12,7 +12,7 @@
 
 void generar_archivo(uint32_t pid);
 void eliminar_archivo(uint32_t pid);
-void escribir_en_archivo(uint32_t pid, int nroMarco);
+void escribir_en_archivo(uint32_t pid, int nroMarco, int nroPagina);
 void* leer_de_archivo(uint32_t pid,int nroMarco);
 char* obtener_path_archivo(uint32_t pid);
 
